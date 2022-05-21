9df2c774e28c9cea4ece3443b1d5b4ea5aae3230
Joseph Murillo <josephmurillo2425@gmail.com>
2022-05-21 15:59:45 -0600

    custom components formik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { TextInput } from "./components/TextInput";
+import { Checkbox } from "./components/Checkbox";
+import { Select } from "./components/Select";
+import { Radio } from "./components/Radio";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = "Requerido";
+  } else if (values.name.length < 5) {
+    errors.name = "El nombre es muy corto";
+  }
+
+  if (!values.lastname) {
+    errors.lastname = "Requerido";
+  } else if (values.lastname.length < 5) {
+    errors.lastname = "El apellido es muy corto";
+  }
+
+  return errors;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      initialValues={{
+        name: "",
+        lastname: "",
+        email: "",
+        chancho: "",
+      }}
+      validate={validate}
+      onSubmit={(values) => console.log(values)}
+    >
+      <Form>
+        <TextInput name="name" label="Nombre" />
+        <br />
+        <TextInput name="lastname" label="Apellido" />
+        <br />
+        <TextInput name="email" label="Correo" />
+        <Select label="Tipo de chancho" name="chancho">
+          <option value="">Seleccione chancho</option>
+          <option value="felipe">Felipe</option>
+          <option value="chanchitofeliz">Chanchito Feliz</option>
+          <option value="chanchitotriste">Chanchito Triste</option>
+        </Select>
+        <Checkbox name="accept">Aceptar terminos y condiciones</Checkbox>
+        <Radio name="radio" value="Chanchito1" label="Chanchito 1" />
+        <Radio name="radio" value="Chanchito2" label="Chanchito 2" />
+        <Radio name="radio" value="Chanchito3" label="Chanchito 3" />
+        <button type="submit">Enviar</button>
+      </Form>
+    </Formik>
   );
 }
 
